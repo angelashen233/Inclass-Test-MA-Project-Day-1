b4b182f477a4b0115edac7885c888821aa85bd1d
Angela Shen <72670483+angelashen233@users.noreply.github.com>
2026-09-16 19:17:26 -0700

    Lady-bug-A1
    A rough shape of the lady bug

--- sketch.js
+++ sketch.js
@@ -5,7 +5,18 @@ function setup() {
 }
 
 function draw() {
-  background(20);
+  background(255, 255, 60);
+  fill(25);
+  circle(100, 70, 20);
   fill(255, 120, 60);
-  circle(mouseX, mouseY, 50);
+  circle(100, 100, 50);
+  fill(20);
+  circle(110, 100, 10);
+  circle(90, 100, 10);
+
+  circle(110, 100 - 14, 10);
+  circle(90, 100 - 14, 10);
+
+  circle(110, 100 + 14, 10);
+  circle(90, 100 + 14, 10);
 }
