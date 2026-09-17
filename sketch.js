08a9f671a907bfab3915092fa10fbb6089606abc
Angela Shen <72670483+angelashen233@users.noreply.github.com>
2026-09-16 18:41:22 -0700

    Prof-requested-updated
    -html
    -Sketch
    -setup
    -Draft

--- sketch.js
+++ sketch.js
@@ -1,1 +1,11 @@
 console.log("I believe I can do it!");
+
+function setup() {
+  createCanvas(800, 600);
+}
+
+function draw() {
+  background(20);
+  fill(255, 120, 60);
+  circle(mouseX, mouseY, 50);
+}
