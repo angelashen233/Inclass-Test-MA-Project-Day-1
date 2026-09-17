a039a5919df19d9ab76fa2a8151381a976d907bf
Angela Shen <72670483+angelashen233@users.noreply.github.com>
2026-09-16 19:23:56 -0700

    Lady bug-update!

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 console.log("I believe I can do it!");
-
+console.log("IT's a ladybug-spider hybrid!");
 function setup() {
   createCanvas(800, 600);
 }
@@ -8,6 +8,16 @@ function draw() {
   background(255, 255, 60);
   fill(25);
   circle(100, 70, 20);
+
+  line(60, 80 - 0, 80 + 20, 90);
+  line(140, 80 - 0, 80 + 20, 90);
+
+  line(140, 80 - 20, 80 + 20, 95);
+  line(60, 80 - 20, 80 + 20, 95);
+
+  line(140 - 20, 80 - 40, 80 + 30, 120);
+  line(60, 80 - 40, 80 + 30, 120);
+
   fill(255, 120, 60);
   circle(100, 100, 50);
   fill(20);
